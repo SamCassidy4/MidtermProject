e852ded285f1283113572892b3d6e918b3042b6e
Sam Cassidy <samuelcassidy1999@gmail.com>
2022-12-21 16:10:35 -0700

    Full-stack smoke test

--- MealMagic/src/main/java/com/skilldistillery/mealmagic/data/UserDAO.java
+++ MealMagic/src/main/java/com/skilldistillery/mealmagic/data/UserDAO.java
@@ -1,5 +1,10 @@
 package com.skilldistillery.mealmagic.data;
 
+import com.skilldistillery.mealmagic.entities.User;
+
 public interface UserDAO {
+	User findById(int userId);
+	User findByUsernameAndPassword(String username, String password);
+	
 
 }
--- MealMagic/src/main/java/com/skilldistillery/mealmagic/data/UserDaoImpl.java
+++ MealMagic/src/main/java/com/skilldistillery/mealmagic/data/UserDaoImpl.java
@@ -6,11 +6,25 @@ import javax.transaction.Transactional;
 
 import org.springframework.stereotype.Service;
 
+import com.skilldistillery.mealmagic.entities.User;
+
 @Service
 @Transactional
 public class UserDaoImpl implements UserDAO {
 
 	@PersistenceContext
 	private EntityManager em;
+
+	@Override
+	public User findById(int userId) {
+		
+		return em.find(User.class, userId);
+	}
+
+	@Override
+	public User findByUsernameAndPassword(String username, String password) {
+		
+		return null;
+	}
 
 }
